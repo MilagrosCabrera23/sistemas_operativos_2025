56dd0bb01367648351e38d7ada1732907b02d29b
Christian Caracach <christiancaracach@gmail.com>
2025-05-08 21:47:17 -0300

    nombre correcto de archivos y placeholders de funciones

---
+++ procesos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Función 1
+funcion1() {
+    # EVE
+    echo "Función 1: Por implementar"
+}
+
+# Función 2
+funcion2() {
+    # FRAN
+    echo "Función 2: Por implementar"
+}
+
+# Función 3
+funcion3() {
+    # MAGUI
+    echo "Función 3: Por implementar"
+}
