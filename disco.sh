be50aeddeb44cd44ae3d67374483de85ef96cf85
Evelin Checa <evecheca40@gmail.com>
2025-06-04 19:56:17 -0300

    tarea

--- disco.sh
+++ disco.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-# Funciones
+# Muestre el espacio libre y utilizado en los sistemas de archivos montados.
+
+mostrar_espacio_disco (){
+    echo " espacio libre y utilizado"
+      df-h
+}
+
+
+#Permita al usuario ingresar una ruta y muestre el tamaño ocupado por ese
+#directorio/archivo (du -sh <ruta>).
+
+mostrar_tamano_ruta(){
+    read -p "Ingrese por favor la ruta del archivo o directorio:" ruta
+
+    if [-e "$ruta"];then
+    du -sh "$ruta"
+    
+    else
+    echo "la ruta indicada no existe"
+
+    fi
+
+}
