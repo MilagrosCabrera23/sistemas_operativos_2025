feb06e8216b55c394559eb69c796a4f9affa13df
MilagrosCabrera <maguicabrera23@gmail.com>
2025-06-05 10:38:56 -0300

    Agregue la funcion de los comandos habituales en el disco

--- disco.sh
+++ disco.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-# Funciones
+# disco_listar_archivos(){
+
+# }
+
+disco_comandos_habituales(){
+echo "Seleccione un comando para ejecutar: df, du,ls"
+read comando
+
+case $comando in
+    df)
+         echo "Mostrando uso del disco:"
+         df -h 
+        ;;
+    du)
+        echo "Mostrando el uso del espacio por carpeta :"
+        du -sh *
+        ;;
+    ls)
+        echo "Listando archivos:"
+        ls -lh
+        ;;
+    *)
+        echo "Comando no reconocido,use alguno de estos: df, du o ls"
+        ;;
+esac
+}
+
+disco_comandos_habituales()
